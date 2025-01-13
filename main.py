80ed3e74d3c2940e1a986ffa6757179495f302fc
Leo <135404395+popcornman209@users.noreply.github.com>
2025-01-12 22:50:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ print("\033c\033[3J\033[95m--DoorDash order bot--\nby Leo :)")
 if "--help" in args:
     print("--days: sets amount of days to check in the past, if not provided you will be asked\n--headless: runs in headless mode, fully in tty\n--help: this page :)")
 else:
+    days = -1
     if "--days" in args:
         days = args[args.index("--days")+1]
         print("loading {} days".format(days))
