766c58ea78a3c869d1ce8090cdbaeea4c3cfa8a1
leo <mail@leofraser.com>
2025-01-14 19:46:01 -0500

    added output argument

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if not os.path.exists("data/"):
 print("\033c\033[3J\033[95m--DoorDash order bot--\nby Leo :)\033[0m")
 
 if "--help" in args:
-    print("--days: sets amount of days to check in the past, if not provided you will be asked\n--headless: runs in headless mode, fully in tty\n--force-display-browser: forces the browser to open\n--help: this page :)")
+    print("--days: sets amount of days to check in the past, if not provided you will be asked\n--headless: runs in headless mode, fully in tty\n--force-display-browser: forces the browser to open\n--output x: saves results to a file, formatted as json\n--help: this page :)")
 else:
     days = -1
     if "--days" in args:
@@ -24,9 +24,13 @@ else:
         print("loading {} days".format(days))
 
     if "--headless" in args:
+        outputFile = None
+        if "--output" in args:
+            outputFile = args[args.index("--output")+1]
+            print("saving output to {}".format(outputFile))
         print("running in headless mode!")
         import lib.mainScript as mScript
-        mScript.main(True,"--force-display-browser" not in args,days)
+        mScript.main(True,"--force-display-browser" not in args,days,savePath=outputFile)
     else:
         import lib.gui
         lib.gui.run()
